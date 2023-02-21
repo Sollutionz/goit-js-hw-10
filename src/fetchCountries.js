3c817350124ed04889d3041273ce69f051c395a0
Sollution <112977952+Sollutionz@users.noreply.github.com>
2023-02-21 18:42:31 +0200

    1

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+export function fetchCountries(name){
+    const ENDPOINT = `https://restcountries.com/v2/name/${name}`;
+
+    return fetch(
+        `${ENDPOINT}?fields=name,capital,flags,population,languages`
+    ).then (response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+
+        return response.json()
+    })
+}
